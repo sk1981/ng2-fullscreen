7b8e061577b039a605b6694826179d98e3d2ad2c
sk1981 <sinavasi@gmail.com>
2016-10-12 23:36:52 +0530

    Integrated demo with angular 2 - router

--- docs/index.ts
+++ docs/index.ts
@@ -1,13 +1,28 @@
 import { platformBrowserDynamic  } from '@angular/platform-browser-dynamic';
+import {RouterModule, Route}   from '@angular/router';
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FullScreenModule } from '../src/index';
+import { FullScreenIfDemoComponent } from './demo_if/FullScreenIfDemoComponent';
 import { BasicComponent } from './demo_basic/BasicComponent';
+import { FullScreenTargetDemoComponent } from './demo_target/FullScreenTargetDemoComponent';
+
+console.log("-----------------------------------------------");
+
+const DemoAppRoutes = [
+  { path: '', component: BasicComponent },
+  { path: 'targetdemo', component: FullScreenTargetDemoComponent },
+  { path: 'ifdemo', component: FullScreenIfDemoComponent }
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FullScreenModule ],
-  declarations: [ BasicComponent ],
-  bootstrap:    [ BasicComponent ]
+  imports:      [
+    BrowserModule,
+    FullScreenModule,
+    RouterModule.forRoot(DemoAppRoutes)
+  ],
+  declarations: [ BasicComponent, FullScreenIfDemoComponent , FullScreenTargetDemoComponent],
+  bootstrap:    [ BasicComponent  ]
 })
 class AppModule { }
 
